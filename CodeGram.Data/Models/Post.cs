726c989057436056e0f566f2cfbbab71f889d569
WELLINGTON  NETO <116044216+net0well@users.noreply.github.com>
2025-02-10 18:58:12 -0300

    Creating a comment model, comments table, and defining relationship in the db

--- CodeGram.Data/Models/Post.cs
+++ CodeGram.Data/Models/Post.cs
@@ -20,6 +20,7 @@ namespace CodeGram.Data.Models
 
         public User User { get; set; }
         public ICollection<Like> Likes { get; set; } = new List<Like>();
+        public ICollection<Comment> Comments { get; set; } = new List<Comment>();
 
     }
 }
