eaab49f0bc399a8e8472d06df19ecc8fda475aa6
WELLINGTON  NETO <116044216+net0well@users.noreply.github.com>
2025-02-12 21:43:12 -0300

    Creating Report Model, Reports table and defining relationships in the db

---
+++ CodeGram.Data/Models/Report.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeGram.Data.Models
+{
+    public class Report
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public int PostId { get; set; }
+        public int UserId { get; set; }
+
+        //navigation properties
+
+        public Post Post { get; set; }
+        public User User { get; set; }
+    }
+}
