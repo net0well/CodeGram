aa0ab58f769ef8d85360b6df850d5055edfb42e7
WELLINGTON  NETO <116044216+net0well@users.noreply.github.com>
2025-02-13 19:47:56 -0300

    implementing post delete confirmation dialog

---
+++ CodeGram.Data/Migrations/20250213215915_Fix_Bugs.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodeGram.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fix_Bugs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
