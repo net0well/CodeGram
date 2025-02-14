b26787b2818ba386ab93950c3e09f3c26d66b10c
WELLINGTON  NETO <116044216+net0well@users.noreply.github.com>
2025-02-14 19:29:19 -0300

    soft delete of posts from the database implemented

--- CodeGram.Data/Models/User.cs
+++ CodeGram.Data/Models/User.cs
@@ -11,6 +11,7 @@ namespace CodeGram.Data.Models
         public int Id { get; set; }
         public string FullName { get; set; }
         public string? ProfilePictureUrl { get; set; }
+        public bool IsDeleted { get; set; }
 
         //Navigation properties
 
