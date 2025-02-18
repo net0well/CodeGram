33e811857c0f009162af79b40dd095b1192a1192
WELLINGTON  NETO <116044216+net0well@users.noreply.github.com>
2025-02-17 23:21:09 -0300

    creating a viewcomponent: loading stories data

---
+++ CodeGram/ViewComponents/StoriesViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using CodeGram.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeGram.ViewComponents
+{
+    public class StoriesViewComponent : ViewComponent
+
+    {
+        private readonly AppDbContext _context;
+        public StoriesViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            var allStories = await _context.Stories.Include(u => u.User).ToListAsync();
+
+            return View(allStories);
+        }
+
+    }
+}
