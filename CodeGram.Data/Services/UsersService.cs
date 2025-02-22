510d76ce30761c0f6f7c1894a491d501b1ce2030
WELLINGTON  NETO <116044216+net0well@users.noreply.github.com>
2025-02-22 17:28:19 -0300

    creating user profile settings service

---
+++ CodeGram.Data/Services/UsersService.cs
@@ -0,0 +1,23 @@
+﻿using CodeGram.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeGram.Data.Services
+{
+    public class UsersService : IUsersService
+    {
+        private readonly AppDbContext _context;
+        public UsersService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<User> GetUser(int loggedInUserId)
+        {
+            return await _context.Users.FirstOrDefaultAsync(n => n.Id == loggedInUserId) ?? new User();
+        }
+    }
+}
