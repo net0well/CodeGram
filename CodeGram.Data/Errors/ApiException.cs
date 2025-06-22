b2747ef6383dbd9c6d41a6aff36504a96997df9e
WELLINGTON  NETO <116044216+net0well@users.noreply.github.com>
2025-06-21 22:22:12 -0300

    adding a custom error middleware and application service extensions

---
+++ CodeGram.Data/Errors/ApiException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeGram.Data.Errors
+{
+    public class ApiException(int statusCode, string message, string? details)
+    {
+        public int SatusCode { get; set; } = statusCode;
+        public string Message { get; set; } = message;
+        public string? Details { get; set; } = details;
+    }
+}
